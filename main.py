b8e090b97aa4905c43db9201f0735f70c57361e2
Lagstill <kpdlakshmi1970@gmail.com>
2023-08-26 17:34:04 +0530

    Authentication added

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import streamlit as st
+import pickle
+from pathlib import Path
 import requests
+import pandas as pd  
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+import streamlit_authenticator as stauth 
+
+
 
 def get_resolution(query):
     api_endpoint = 'https://socgen.azurewebsites.net/query'
@@ -13,8 +20,45 @@ def get_resolution(query):
     else:
         return 'Error fetching resolution'
 
-def main():
-    st.title('Error Resolution System')
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+
+names = ["Alagu Prakalaya", "Pragathi"]
+usernames = ["alagu", "pragathi"]
+
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+
+credentials = {
+    "usernames":{
+        usernames[0]:{
+            "name":names[0],
+            "password":hashed_passwords[0]
+            },
+        usernames[1]:{
+            "name":names[1],
+            "password":hashed_passwords[1]
+            }            
+        }
+    }
+
+
+# load hashed passwords
+
+
+authenticator = stauth.Authenticate(credentials,"incident_management_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
+    st.title('Incident Management System')
     
     error_types = [
         'FTR', 'Essbase', 'Database', 'Application', 'Server Outage',
@@ -22,7 +66,7 @@ def main():
         'Application Performance Drop', 'Data Import Failure'
     ]
     
-    selected_error_type = st.selectbox('Select Error Type:', error_types)
+    # selected_error_type = st.selectbox('Select Error Type:', error_types)
     user_query = st.text_input('Enter Your Query:')
     
     if st.button('Get Resolution'):
@@ -33,5 +77,4 @@ def main():
         else:
             st.warning('Please enter a query.')
 
-if __name__ == '__main__':
-    main()
+
