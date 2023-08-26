9724870b767e360606cd53cdeecda6d4598c03ca
Lagstill <kpdlakshmi1970@gmail.com>
2023-08-26 17:54:26 +0530

    Updated

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import pickle
-from pathlib import Path
+import streamlit as st
 import requests
-# import pandas as pd  
-# import plotly.express as px  # pip install plotly-express
-import subprocess
-import streamlit as st  # pip install streamlit
-import streamlit_authenticator as stauth 
-
-subprocess.run(["pip", "install", "streamlit-authenticator"])
-
 
 def get_resolution(query):
     api_endpoint = 'https://socgen.azurewebsites.net/query'
@@ -22,45 +13,8 @@ def get_resolution(query):
     else:
         return 'Error fetching resolution'
 
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
-
-names = ["Alagu Prakalaya", "Pragathi"]
-usernames = ["alagu", "pragathi"]
-
-file_path = Path(__file__).parent / "hashed_pw.pkl"
-with file_path.open("rb") as file:
-    hashed_passwords = pickle.load(file)
-
-
-credentials = {
-    "usernames":{
-        usernames[0]:{
-            "name":names[0],
-            "password":hashed_passwords[0]
-            },
-        usernames[1]:{
-            "name":names[1],
-            "password":hashed_passwords[1]
-            }            
-        }
-    }
-
-
-# load hashed passwords
-
-
-authenticator = stauth.Authenticate(credentials,"incident_management_dashboard", "abcdef", cookie_expiry_days=30)
-
-name, authentication_status, username = authenticator.login("Login", "main")
-
-if authentication_status == False:
-    st.error("Username/password is incorrect")
-
-if authentication_status == None:
-    st.warning("Please enter your username and password")
-
-if authentication_status:
-    st.title('Incident Management System')
+def main():
+    st.title('Error Resolution System')
     
     error_types = [
         'FTR', 'Essbase', 'Database', 'Application', 'Server Outage',
@@ -79,4 +33,5 @@ if authentication_status:
         else:
             st.warning('Please enter a query.')
 
-
+if __name__ == '__main__':
+    main()
